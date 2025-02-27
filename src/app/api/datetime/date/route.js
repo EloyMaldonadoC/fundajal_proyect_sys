0cbeebff3d565579b191d422f136a931a9c2c119
Eloy Maldonado Calzada <eloymaldonado85@gmail.com>
2025-02-27 15:17:13 -0600

    modificaciones a api/datetime

--- src/app/api/datetime/date/route.js
+++ src/app/api/datetime/date/route.js
@@ -2,7 +2,13 @@ import { NextResponse } from "next/server";
 import { connection } from "@/libs/mysql";
 
 export async function GET() {
-    process.env.TZ = 'America/Mexico/Guadalajara'
-    const result = await connection.query(`SELECT DATE_FORMAT(NOW(), '%Y-%m-%d') AS fecha_actual`);
-    return NextResponse.json(result[0])
+  try {
+    process.env.TZ = "America/Mexico/Guadalajara";
+    const result = await connection.query(
+      `SELECT DATE_FORMAT(NOW(), '%Y-%m-%d') AS fecha_actual`
+    );
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.error({ message: error.message }, { status: 500 });
+  }
 }
--- src/app/api/datetime/route.js
+++ src/app/api/datetime/route.js
@@ -2,7 +2,13 @@ import { NextResponse } from "next/server";
 import { connection } from "@/libs/mysql";
 
 export async function GET() {
-    process.env.TZ = 'America/Mexico/Guadalajara'
-    const result = await connection.query(`SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') AS fecha_hora_actual`);
-    return NextResponse.json(result[0])
+  try {
+    process.env.TZ = "America/Mexico/Guadalajara";
+    const result = await connection.query(
+      `SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') AS fecha_hora_actual`
+    );
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.error({ message: error.message }, { status: 500 });
+  }
 }
