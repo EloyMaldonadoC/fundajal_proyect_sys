b6561ba9f63b4104208a15a062bdf41fbfa4889b
Eloy Maldonado Calzada <eloymaldonado85@gmail.com>
2025-02-27 15:03:00 -0600

    Re ajuste a src/components/input/InputComponents.js

--- src/components/input/InputComponents.js
+++ src/components/input/InputComponents.js
@@ -7,7 +7,7 @@ import Table from "./Table";
 import Info from "./Info";
 import Select from "./Select";
 import TableEmp from "./TableEmp";
-import InfoEmp from "./infoEmp";
+import InfoEmp from "./InfoEmp";
 import CheckBox from "./CheckBox";
 
 export { Input, Modal, Button, Search, Alert, Table, Info, Select, TableEmp, InfoEmp, CheckBox };
