396bb70b89192d7e3c0b9aeff961f8c22585ad54
Eloy Maldonado Calzada <eloymaldonado85@gmail.com>
2025-06-24 14:55:09 -0600

    filtros para entregas

--- src/app/api/entregas/cardEntregas/route.js
+++ src/app/api/entregas/cardEntregas/route.js
@@ -8,14 +8,28 @@ export async function GET(req) {
     const limit = url.searchParams.get("limit") || 30;
     const filter = url.searchParams.get("filter") || null;
     const user = url.searchParams.get("user") || null;
-
+    const filtro_fecha = url.searchParams.get("filtro_fecha") || null;
+    const modo = url.searchParams.get("modo") || null;
+    
     const offset = (page - 1) * limit;
     // Realiza consultas independientes para cada tabla
     const [entregas, vehiculos, empleados, clientes, entrega_vehiculo, empleado_entraga] = await connection.query(
-      `SELECT * FROM entregas 
+      `SELECT * 
+      FROM entregas
       ${filter ? `WHERE en_estado LIKE "${filter}"` : ""} 
       ${filter && user ? `AND emp_id = ${user}` : user ? `WHERE emp_id = ${user}` : ""}
-      ORDER BY en_dia_pedido DESC LIMIT ${limit} OFFSET ${offset};
+      ${(filter || user) && filtro_fecha ? "AND" : filtro_fecha && !(filter || user) ? "WHERE" : ""}
+      ${filtro_fecha && modo === 'dia' 
+          ? `DATE(en_dia_entrega) = DATE('${filtro_fecha}')`
+          : filtro_fecha && modo === 'semana' 
+            ? `YEARWEEK(en_dia_entrega, 1) = YEARWEEK('${filtro_fecha}', 1)`
+            : filtro_fecha && modo === 'mes'
+              ? `YEAR(en_dia_entrega) = YEAR('${filtro_fecha}') AND MONTH(en_dia_entrega) = MONTH('${filtro_fecha}')`
+              : ""}
+      ORDER BY 
+        CASE WHEN en_dia_entrega IS NULL THEN 0 ELSE 1 END,
+        en_dia_entrega DESC
+      LIMIT ${limit} OFFSET ${offset};
       SELECT * FROM vehiculos;
       SELECT emp_id, emp_nombre, emp_apellido, emp_num_tel, emp_rol, 
       emp_estado, emp_hora_entrada, emp_hora_salida, emp_foto, emp_usuario FROM empleados;
